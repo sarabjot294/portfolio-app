29894a0355933dd294ab535639a517de837278e3
sarabjot294 <sarabot294@gmail.com>
2022-01-08 03:48:49 -0800

    Added icon data to the projects.

--- lib/config/data/projects.dart
+++ lib/config/data/projects.dart
@@ -5,47 +5,13 @@ import 'package:portfolio/model/tiles_model.dart';
 
 final List<TilesData> awards = [
   TilesData(
-      name: 'mBillionth South Asia Award',
-      image: 'assests/images/awards/mbillion.png',
-      imageLight: 'assests/images/awards/mbillion.png',
-      description:
-          "TaanaBaana Mobile App won a special mention award in 'Inclusion & Empowerment' category in New delhi in Feburary 2020",
-      link: 'http://mbillionth.in/taana-baana-2/',
-      smallIcons: [
-        //This is a test data.
-        SmallIcons(
-            icon: Icon(
-              Icons.laptop,
-              size: 15.0,
-              color: kAccentColorLight,
-              semanticLabel: "Web Application",
-            ),
-            tooltip: "Web Application"),
-        SmallIcons(
-            icon: Icon(
-              Icons.stay_primary_portrait_outlined,
-              size: 15.0,
-              color: kAccentColorLight,
-              semanticLabel: "Mobile Application",
-            ),
-            tooltip: "Mobile Application"),
-        SmallIcons(
-            icon: Icon(
-              Icons.cloud_circle,
-              size: 15.0,
-              color: kAccentColorLight,
-              semanticLabel: "Cloud Application",
-            ),
-            tooltip: "Cloud Application"),
-        SmallIcons(
-            icon: Icon(
-              Icons.desktop_mac,
-              size: 15.0,
-              color: kAccentColorLight,
-              semanticLabel: "Desktop Application",
-            ),
-            tooltip: "Desktop Application"),
-      ]),
+    name: 'mBillionth South Asia Award',
+    image: 'assests/images/awards/mbillion.png',
+    imageLight: 'assests/images/awards/mbillion.png',
+    description:
+        "TaanaBaana Mobile App won a special mention award in 'Inclusion & Empowerment' category in New delhi in Feburary 2020",
+    link: 'http://mbillionth.in/taana-baana-2/',
+  ),
   TilesData(
       name: 'Secretary - General',
       image: 'assests/images/awards/jecrcmun.png',
@@ -86,7 +52,26 @@ final List<TilesData> projects = [
     image: 'assests/images/projects/osm.png',
     imageLight: 'assests/images/projects/osm.png',
     description:
-        'Worked closely with Microsoft on improving road connectivity for Open Street Maps, using graph algorithms like Dijkstra, Breadth first search, and Prim\'s minimum spanning tree. Worked with Geo-Spatial datasets.',
+        'Worked closely with Microsoft on improving road connectivity for Open Street Maps, using graph algorithms like Dijkstra, Breadth first search, and Prim\'s minimum spanning tree.',
+    smallIcons: [
+      //This is a test data.
+      SmallIcons(
+          icon: Icon(
+            Icons.laptop,
+            size: 15.0,
+            color: kAccentColorLight,
+            semanticLabel: "Web Application",
+          ),
+          tooltip: "Web Application"),
+      SmallIcons(
+          icon: Icon(
+            Icons.cloud_circle,
+            size: 15.0,
+            color: kAccentColorLight,
+            semanticLabel: "Cloud Application",
+          ),
+          tooltip: "Cloud Application"),
+    ],
   ),
   TilesData(
       name: 'TaanaBaana',
@@ -94,89 +79,240 @@ final List<TilesData> projects = [
       imageLight: 'assests/images/projects/taanabaana.png',
       description:
           "An android app with multiple user role logins used by supply chain. It has various features like barcode scanning, image capture, GPS, firebase crashanalytics, and etc.",
-      link: 'http://mbillionth.in/taana-baana-2/'),
+      link: 'http://mbillionth.in/taana-baana-2/',
+      smallIcons: [
+        SmallIcons(
+            icon: Icon(
+              Icons.stay_primary_portrait_outlined,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Mobile Application",
+            ),
+            tooltip: "Mobile Application"),
+        SmallIcons(
+            icon: Icon(
+              Icons.cloud_circle,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Cloud Application",
+            ),
+            tooltip: "Cloud Application"),
+        SmallIcons(
+            icon: Icon(
+              Icons.laptop,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Web Application",
+            ),
+            tooltip: "Web Application"),
+      ]),
   TilesData(
-    name: 'Portolio',
-    image: 'assests/images/projects/portfolio.png',
-    imageLight: 'assests/images/projects/portfolio.png',
-    description:
-        'A Hobby. Created a portfolio app with bottom navigation bar, animations, listView, images, icons, external links, themes, firebase analytics intregation and a clean UI.',
-  ),
+      name: 'Portolio',
+      image: 'assests/images/projects/portfolio.png',
+      imageLight: 'assests/images/projects/portfolio.png',
+      description:
+          'A Hobby. Created a portfolio app with bottom navigation bar, animations, listView, images, icons, external links, themes, firebase analytics intregation and a clean UI.',
+      smallIcons: [
+        SmallIcons(
+            icon: Icon(
+              Icons.laptop,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Web Application",
+            ),
+            tooltip: "Web Application"),
+        SmallIcons(
+            icon: Icon(
+              Icons.stay_primary_portrait_outlined,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Mobile Application",
+            ),
+            tooltip: "Mobile Application"),
+      ]),
   TilesData(
-    name: 'Crystalline',
-    image: 'assests/images/projects/crystaline.png',
-    imageLight: 'assests/images/projects/crystaline.png',
-    description:
-        'Created smart lamp with home assistant using Raspberry Pi for Internet of Things coursework at UW. Responsible for creating backend architecture using IBM Watson, Grafana, and Node-red.',
-  ),
+      name: 'Crystalline',
+      image: 'assests/images/projects/crystaline.png',
+      imageLight: 'assests/images/projects/crystaline.png',
+      description:
+          'Created smart lamp with home assistant using Raspberry Pi for Internet of Things coursework at UW. Responsible for creating backend architecture using IBM Watson, Grafana, and Node-red.',
+      smallIcons: [
+        SmallIcons(
+            icon: Icon(
+              Icons.desktop_mac,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Desktop Application",
+            ),
+            tooltip: "Desktop Application"),
+      ]),
   TilesData(
-    name: 'Online Spam Detection',
-    image: 'assests/images/projects/portfolio.png',
-    imageLight: 'assests/images/projects/portfolio.png',
-    description:
-        'Developed multiple machine learning models to detect false reviews on the hotel website. Performed data analysis, data cleaning, data pre-processing, model training, and fine-tuning.',
-  ),
+      name: 'Online Spam Detection',
+      image: 'assests/images/projects/portfolio.png',
+      imageLight: 'assests/images/projects/portfolio.png',
+      description:
+          'Developed multiple machine learning models to detect false reviews on the hotel website. Performed data analysis, data cleaning, data pre-processing, model training, and fine-tuning.',
+      smallIcons: [
+        SmallIcons(
+            icon: Icon(
+              Icons.desktop_mac,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Desktop Application",
+            ),
+            tooltip: "Desktop Application"),
+      ]),
   TilesData(
-    name: 'JR-Live',
-    image: 'assests/images/projects/jrlive.png',
-    imageLight: 'assests/images/projects/jrlive.png',
-    description:
-        'An ecommerce app built for internal sales team for JR. It has various features like sort, filter, search and add to favourites, creating quotation and invoicing in PDF and Excel.',
-  ),
+      name: 'JR-Live',
+      image: 'assests/images/projects/jrlive.png',
+      imageLight: 'assests/images/projects/jrlive.png',
+      description:
+          'An ecommerce app built for internal sales team for JR. It has various features like sort, filter, search and add to favourites, creating quotation and invoicing in PDF and Excel.',
+      smallIcons: [
+        SmallIcons(
+            icon: Icon(
+              Icons.stay_primary_portrait_outlined,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Mobile Application",
+            ),
+            tooltip: "Mobile Application"),
+        SmallIcons(
+            icon: Icon(
+              Icons.cloud_circle,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Cloud Application",
+            ),
+            tooltip: "Cloud Application"),
+      ]),
   TilesData(
-    name: 'Multinode Key-Value Store',
-    image: 'assests/images/projects/portfolio.png',
-    imageLight: 'assests/images/projects/portfolio.png',
-    description:
-        'Created a multi-node cluster of servers using Docker for \‘Advance Distributed Systems\’ coursework. Implemented 2-phase commit for \‘put\’ and \‘delete\’ operations with thread-safe parallel programming.',
-  ),
+      name: 'Multinode Key-Value Store',
+      image: 'assests/images/projects/portfolio.png',
+      imageLight: 'assests/images/projects/portfolio.png',
+      description:
+          'Created a multi-node cluster of servers using Docker for \‘Advance Distributed Systems\’ coursework. Implemented 2-phase commit for \‘put\’ and \‘delete\’ operations with thread-safe parallel programming.',
+      smallIcons: [
+        SmallIcons(
+            icon: Icon(
+              Icons.cloud_circle,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Cloud Application",
+            ),
+            tooltip: "Cloud Application"),
+      ]),
   TilesData(
-    name: 'JRF field-work',
-    image: 'assests/images/projects/field_work.png',
-    imageLight: 'assests/images/projects/field_work.png',
-    description:
-        'An android application used by JR Foundation team to manage all work flow and provides analytics of each project.',
-  ),
+      name: 'JRF field-work',
+      image: 'assests/images/projects/field_work.png',
+      imageLight: 'assests/images/projects/field_work.png',
+      description:
+          'An android application used by JR Foundation team to manage all work flow and provides analytics of each project.',
+      smallIcons: [
+        SmallIcons(
+            icon: Icon(
+              Icons.stay_primary_portrait_outlined,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Mobile Application",
+            ),
+            tooltip: "Mobile Application"),
+      ]),
   TilesData(
-    name: 'JR-Mirzapur',
-    image: 'assests/images/projects/mirzapur.png',
-    imageLight: 'assests/images/projects/mirzapur.png',
-    description:
-        'An mobile application linked with Microdoft dynamic NAV through an intermidiate Linux server for managing orders in Jaipur Rugs. Created the whole mobile application and backend with PHP Laravel.',
-  ),
+      name: 'JR-Mirzapur',
+      image: 'assests/images/projects/mirzapur.png',
+      imageLight: 'assests/images/projects/mirzapur.png',
+      description:
+          'An mobile application linked with Microdoft dynamic NAV through an intermidiate Linux server for managing orders in Jaipur Rugs. Created the whole mobile application and backend with PHP Laravel.',
+      smallIcons: [
+        SmallIcons(
+            icon: Icon(
+              Icons.stay_primary_portrait_outlined,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Mobile Application",
+            ),
+            tooltip: "Mobile Application"),
+      ]),
   TilesData(
-    name: 'JRC Store-Management',
-    image: 'assests/images/projects/store.png',
-    imageLight: 'assests/images/projects/store.png',
-    description:
-        'An inventory management app created in Flutter with Barcode functionality to keep track of items at multiple locations through a centeralized database',
-  ),
+      name: 'JRC Store-Management',
+      image: 'assests/images/projects/store.png',
+      imageLight: 'assests/images/projects/store.png',
+      description:
+          'An inventory management app created in Flutter with Barcode functionality to keep track of items at multiple locations through a centeralized database',
+      smallIcons: [
+        SmallIcons(
+            icon: Icon(
+              Icons.stay_primary_portrait_outlined,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Mobile Application",
+            ),
+            tooltip: "Mobile Application"),
+      ]),
   TilesData(
-    name: 'SoochnaPrenuer',
-    image: 'assests/images/projects/soochna.png',
-    imageLight: 'assests/images/projects/soochna.png',
-    description:
-        'An android mobile application to capture all the image, audio, video data straight from the field at high quality. It is intregated with FTP to transfer MIME directly to server.',
-  ),
+      name: 'SoochnaPrenuer',
+      image: 'assests/images/projects/soochna.png',
+      imageLight: 'assests/images/projects/soochna.png',
+      description:
+          'An android mobile application to capture all the image, audio, video data straight from the field at high quality. It is intregated with FTP to transfer MIME directly to server.',
+      smallIcons: [
+        SmallIcons(
+            icon: Icon(
+              Icons.stay_primary_portrait_outlined,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Mobile Application",
+            ),
+            tooltip: "Mobile Application"),
+      ]),
   TilesData(
       name: 'League de Alok',
       image: 'assests/images/projects/alok.png',
       imageLight: 'assests/images/projects/alok.png',
       description:
           'A racing bar graph created with 2019-20 FPL season for our mini league \'Liga de Alok\' just for fun.',
-      link: 'https://public.flourish.studio/visualisation/3255550/'),
+      link: 'https://public.flourish.studio/visualisation/3255550/',
+      smallIcons: [
+        SmallIcons(
+            icon: Icon(
+              Icons.laptop,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Web Application",
+            ),
+            tooltip: "Web Application"),
+      ]),
   TilesData(
-    name: 'JRF Survey',
-    image: 'assests/images/projects/survey.png',
-    imageLight: 'assests/images/projects/survey.png',
-    description:
-        'A survey app created for JR Foundation team, to link each artisans in the systems. Multiple features like, offline mode, aadhar scanning and etc.',
-  ),
+      name: 'JRF Survey',
+      image: 'assests/images/projects/survey.png',
+      imageLight: 'assests/images/projects/survey.png',
+      description:
+          'A survey app created for JR Foundation team, to link each artisans in the systems. Multiple features like, offline mode, aadhar scanning and etc.',
+      smallIcons: [
+        SmallIcons(
+            icon: Icon(
+              Icons.desktop_mac,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Desktop Application",
+            ),
+            tooltip: "Desktop Application"),
+      ]),
   TilesData(
-    name: 'Binge Watch',
-    image: 'assests/images/projects/binge.png',
-    imageLight: 'assests/images/projects/binge.png',
-    description:
-        'A desktop application which provides user tailor made movie recommendations. It uses ML alogirthm to suggest top 3 movies based upon user\'s past experience.',
-  ),
+      name: 'Binge Watch',
+      image: 'assests/images/projects/binge.png',
+      imageLight: 'assests/images/projects/binge.png',
+      description:
+          'A desktop application which provides user tailor made movie recommendations. It uses ML alogirthm to suggest top 3 movies based upon user\'s past experience.',
+      smallIcons: [
+        SmallIcons(
+            icon: Icon(
+              Icons.desktop_mac,
+              size: 15.0,
+              color: kAccentColorLight,
+              semanticLabel: "Desktop Application",
+            ),
+            tooltip: "Desktop Application"),
+      ])
 ];
